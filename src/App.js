6c106c6d252e8a6c85bc3a1dc3b07ad9c8eea4b5
Alfredo <alf_baza@hotmail.com>
2024-02-01 01:02:21 -0500

    Simple Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import img from './Media/01.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Blog of the underground pavement</h1>
+      <p>Once upon a time there was a bunch of coders</p>
+      <p>They tried to do some stupid blog</p>
+      <p>But they couldn't do it and so they had to learn tutorials online.</p>
+      <p>Goddamn the page is not even resizable but this is how we start coding.</p>
+      <div className='bg'></div>
     </div>
+
   );
 }
 
